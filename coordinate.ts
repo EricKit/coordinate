6a8c30ec919c5634289e4b442495516ae7fdc54d
Eric Kitaif <eric.kitaif@gmail.com>
2021-09-15 19:40:21 -0400

    Update coordinate.ts
    Fixed a bug - a while ago - not sure what it was

--- coordinate.ts
+++ coordinate.ts
@@ -1,11 +1,13 @@
 import { roundAndTruncate } from '../lib/lib';
-import mgrs from 'mgrs';
+import mgrs from '../projects/mgrs/dist/mgrs';
 
 export type CoordinateFormat = 'D.D' | 'D M.M' | 'D M S.S' | 'MGRS';
 
 export class Coordinate {
-  latitude: number;
-  longitude: number;
+  latitudeDecimal: number;
+  longitudeDecimal: number;
+  latitudeWhole: number;
+  longitudeWhole: number;
   // To know how a coordinate was created if passed in a string, otherwise it is
   // not detected
   detected: CoordinateFormat | 'Not Detected';
@@ -20,8 +22,14 @@ export class Coordinate {
     // If passed a lat and lng as a number, create a new coordinate. It sets
     // the detected to Not Detected. Detected is for user input strings.
     if (typeof latOrString !== 'string' && typeof longitude === 'number') {
-      this.latitude = latOrString;
-      this.longitude = longitude;
+      if (latOrString > 90 || latOrString < -90 || longitude > 180 || longitude < -180)
+        throw new Error('Not valid coordinates');
+      this.latitudeWhole = Math.floor(Math.abs(latOrString));
+      this.longitudeWhole = Math.floor(Math.abs(longitude));
+      this.latitudeDecimal = latOrString % 1;
+      this.longitudeDecimal = longitude % 1;
+      if (latOrString < 0) this.latitudeWhole *= -1;
+      if (longitude < 0) this.longitudeWhole *= -1;
       this.detected = 'Not Detected';
       return;
     }
@@ -47,9 +55,15 @@ export class Coordinate {
         }
       }
 
-      const [lon, lat] = mgrs.toPoint(string);
-      this.latitude = lat;
-      this.longitude = lon;
+      const [lng, lat] = mgrs.toPoint(string);
+      this.latitudeWhole = Math.floor(Math.abs(lat));
+      this.longitudeWhole = Math.floor(Math.abs(lng));
+      this.latitudeDecimal = lat % 1;
+      this.longitudeDecimal = lng % 1;
+      if (lat < 0 && this.latitudeWhole > 0) this.latitudeWhole = this.latitudeWhole * -1;
+      if (lng < 0 && this.longitudeWhole > 0) this.longitudeWhole = this.longitudeWhole * -1;
+      if (this.latitudeWhole < 0 && this.longitudeWhole > 0) this.longitudeWhole = this.longitudeWhole * -1;
+      if (this.longitudeWhole < 0 && this.longitudeDecimal > 0) this.longitudeDecimal = this.longitudeDecimal * -1;
       this.detected = 'MGRS';
       return;
     }
@@ -58,42 +72,64 @@ export class Coordinate {
     // wanting perfectly formatted coordinates. It searches for W or E in the string and
     // looks at the number of digits grouped together.
     const containsWest = coordinate.match(/[wW]+/) ? true : false;
-    const containesSouth = coordinate.match(/[sS]+/) ? true : false;
+    const containsSouth = coordinate.match(/[sS]+/) ? true : false;
     const parts = coordinate.match(/-?[0-9.]+/g);
     if (!parts) throw new Error('Not valid coordinates');
     const numbers = parts.map((part) => parseFloat(part));
 
     // The user must be passing in D.D with only two sets of numbers
     if (numbers.length === 2) {
-      this.latitude = numbers[0];
-      this.longitude = numbers[1];
-      if (containesSouth && this.latitude > 0) this.latitude = this.latitude * -1;
-      if (containsWest && this.longitude > 0) this.longitude = this.longitude * -1;
+      this.latitudeWhole = Math.floor(Math.abs(numbers[0]));
+      this.longitudeWhole = Math.floor(Math.abs(numbers[1]));
+      this.latitudeDecimal = numbers[0] % 1;
+      this.longitudeDecimal = numbers[1] % 1;
+      if ((containsSouth || numbers[0] < 0) && this.latitudeWhole > 0) this.latitudeWhole = this.latitudeWhole * -1;
+      if ((containsWest || numbers[1] < 0) && this.longitudeWhole > 0) this.longitudeWhole = this.longitudeWhole * -1;
+      if (this.latitudeWhole < 0 && this.latitudeDecimal > 0) this.latitudeDecimal = this.latitudeDecimal * -1;
+      if (this.longitudeWhole < 0 && this.longitudeDecimal > 0) this.longitudeDecimal = this.longitudeDecimal * -1;
       this.detected = 'D.D';
 
       // The user is passing in D M.M
     } else if (numbers.length === 4) {
-      if (numbers[1] < 0 || numbers[3] < 0) throw new Error('Not valid coordinates');
-      this.latitude = Math.abs(numbers[0]) + numbers[1] / 60;
-      this.longitude = Math.abs(numbers[2]) + numbers[3] / 60;
-      if (containesSouth && numbers[0] > 0) this.latitude = this.latitude * -1;
-      if (containsWest && numbers[2] > 0) this.longitude = this.longitude * -1;
+      if (numbers[1] < 0 || numbers[3] < 0 || numbers[1] > 60 || numbers[3] > 60)
+        throw new Error('Not valid coordinates');
+      this.latitudeWhole = Math.floor(numbers[0]);
+      this.longitudeWhole = Math.floor(numbers[2]);
+      this.latitudeDecimal = numbers[1] / 60;
+      this.longitudeDecimal = numbers[3] / 60;
+      if ((containsSouth || numbers[0] < 0) && this.latitudeWhole > 0) this.latitudeWhole = this.latitudeWhole * -1;
+      if ((containsWest || numbers[2] < 0) && this.longitudeWhole > 0) this.longitudeWhole = this.longitudeWhole * -1;
+      if (this.latitudeWhole < 0 && this.latitudeDecimal > 0) this.latitudeDecimal = this.latitudeDecimal * -1;
+      if (this.longitudeWhole < 0 && this.longitudeDecimal > 0) this.longitudeDecimal = this.longitudeDecimal * -1;
       this.detected = 'D M.M';
 
       // The user is passing in D M S.S
     } else if (numbers.length === 6) {
-      if (numbers[1] < 0 || numbers[2] < 0 || numbers[4] < 0 || numbers[5] < 0)
+      if (
+        numbers[1] < 0 ||
+        numbers[2] < 0 ||
+        numbers[4] < 0 ||
+        numbers[5] < 0 ||
+        numbers[1] > 60 ||
+        numbers[2] > 60 ||
+        numbers[4] > 60 ||
+        numbers[5] > 60
+      )
         throw new Error('Not valid coordinates');
-      this.latitude = Math.abs(numbers[0]) + numbers[1] / 60 + numbers[2] / 60 / 60;
-      this.longitude = Math.abs(numbers[3]) + numbers[4] / 60 + numbers[5] / 60 / 60;
-      if (containesSouth && numbers[0] > 0) this.latitude = this.latitude * -1;
-      if (containsWest && numbers[3] > 0) this.longitude = this.longitude * -1;
+      this.latitudeWhole = Math.floor(numbers[0]);
+      this.longitudeWhole = Math.floor(numbers[3]);
+      this.latitudeDecimal = numbers[1] / 60 + numbers[2] / 3600;
+      this.longitudeDecimal = numbers[4] / 60 + numbers[5] / 3600;
+      if ((containsSouth || numbers[0] < 0) && this.latitudeWhole > 0) this.latitudeWhole = this.latitudeWhole * -1;
+      if ((containsWest || numbers[3] < 0) && this.longitudeWhole > 0) this.longitudeWhole = this.longitudeWhole * -1;
+      if (this.latitudeWhole < 0 && this.latitudeDecimal > 0) this.latitudeDecimal = this.latitudeDecimal * -1;
+      if (this.longitudeWhole < 0 && this.longitudeDecimal > 0) this.longitudeDecimal = this.longitudeDecimal * -1;
       this.detected = 'D M S.S';
     } else {
       throw new Error('Not valid coordinates');
     }
 
-    if (this.latitude > 90 || this.latitude < -90 || this.longitude > 180 || this.longitude < -180)
+    if (this.latitudeWhole > 90 || this.latitudeWhole < -90 || this.longitudeWhole > 180 || this.longitudeWhole < -180)
       throw new Error('Not valid coordinates');
   }
 
@@ -107,30 +143,48 @@ export class Coordinate {
       case 'D M S.S':
         return this.degreesMinutesSecondsDecimalSeconds;
       case 'MGRS':
-        let string = mgrs.forward([this.longitude, this.latitude]);
+        let string = mgrs.forward([this.longitude, this.latitude], 5);
         string = string.slice(0, 3) + ' ' + string.slice(3, 5) + ' ' + string.slice(5, 10) + ' ' + string.slice(10);
         return string;
     }
   }
 
+  toLatLonObject(): { latitude: number; longitude: number } {
+    return { latitude: this.latitude, longitude: this.longitude };
+  }
+
+  get latitude() {
+    return this.latitudeWhole + this.latitudeDecimal;
+  }
+
+  get longitude() {
+    return this.longitudeWhole + this.longitudeDecimal;
+  }
+
   // Returns N 34.54 W 113.24
   get degreesDecimalDegrees() {
-    const latDeg = Math.abs(this.latitude);
-    const lngDeg = Math.abs(this.longitude);
+    const latDeg = Math.abs(this.latitudeWhole);
+    const lngDeg = Math.abs(this.longitudeWhole);
     let latHemisphere = 'N';
     let lngHemisphere = 'E';
-    if (this.latitude < 0) latHemisphere = 'S';
-    if (this.longitude < 0) lngHemisphere = 'W';
+    if (this.latitudeWhole < 0) latHemisphere = 'S';
+    if (this.longitudeWhole < 0) lngHemisphere = 'W';
 
-    return `${latHemisphere} ${roundAndTruncate(latDeg, 6)} ` + `${lngHemisphere} ${roundAndTruncate(lngDeg, 6)}`;
+    const latDecimalSplit = roundAndTruncate(this.latitudeDecimal, 6).split('.');
+    let latDecimal = latDecimalSplit?.[1] ?? '';
+    if (latDecimal) latDecimal = '.' + latDecimal;
+
+    const lngDecimalSplit = roundAndTruncate(this.longitudeDecimal, 6).split('.');
+    let lngDecimal = lngDecimalSplit?.[1] ?? '';
+    if (lngDecimal) lngDecimal = '.' + lngDecimal;
+
+    return `${latHemisphere} ${latDeg}${latDecimal} ` + `${lngHemisphere} ${lngDeg}${lngDecimal}`;
   }
 
   // Returns N 34 50.2 W 116 32.4
   get degreesMinutesDecimalMinutes() {
-    const latDegFloor = Math.floor(Math.abs(this.latitude));
-    const latMin = latDegFloor === 0 ? this.latitude * 60 : (Math.abs(this.latitude) % latDegFloor) * 60;
-    const lngDegFloor = Math.floor(Math.abs(this.longitude));
-    const lngMin = lngDegFloor === 0 ? this.longitude * 60 : (Math.abs(this.longitude) % lngDegFloor) * 60;
+    const latMin = Math.abs(this.latitudeDecimal * 60);
+    const lngMin = Math.abs(this.longitudeDecimal * 60);
     let latHemisphere = 'N';
     let lngHemisphere = 'E';
     if (this.latitude < 0) {
@@ -140,22 +194,20 @@ export class Coordinate {
       lngHemisphere = 'W';
     }
     return (
-      `${latHemisphere} ${latDegFloor} ${roundAndTruncate(latMin, 4, 2)} ` +
-      `${lngHemisphere} ${lngDegFloor} ${roundAndTruncate(lngMin, 4, 2)}`
+      `${latHemisphere} ${Math.abs(this.latitudeWhole)} ${roundAndTruncate(latMin, 4, 2)} ` +
+      `${lngHemisphere} ${Math.abs(this.longitudeWhole)} ${roundAndTruncate(lngMin, 4, 2)}`
     );
   }
 
   // Returns N 39 50 24.23 W 115 34 23.9
   get degreesMinutesSecondsDecimalSeconds() {
-    const latDegFloor = Math.floor(Math.abs(this.latitude));
-    const latMin = latDegFloor === 0 ? this.latitude * 60 : (Math.abs(this.latitude) % latDegFloor) * 60;
+    const latMin = Math.abs(this.latitudeDecimal) * 60;
     const latMinFloor = Math.floor(latMin);
-    const latSec = latMinFloor === 0 ? latMinFloor * 60 : (latMin % latMinFloor) * 60;
+    const latSec = latMinFloor === 0 ? latMin * 60 : (latMin % latMinFloor) * 60;
 
-    const lngDegFloor = Math.floor(Math.abs(this.longitude));
-    const lngMin = lngDegFloor === 0 ? this.longitude * 60 : (Math.abs(this.longitude) % lngDegFloor) * 60;
+    const lngMin = Math.abs(this.longitudeDecimal) * 60;
     const lngMinFloor = Math.floor(lngMin);
-    const lngSec = lngMinFloor === 0 ? lngMinFloor * 60 : (lngMin % lngMinFloor) * 60;
+    const lngSec = lngMinFloor === 0 ? lngMin * 60 : (lngMin % lngMinFloor) * 60;
     let latHemisphere = 'N';
     let lngHemisphere = 'E';
     if (this.latitude < 0) {
@@ -165,8 +217,16 @@ export class Coordinate {
       lngHemisphere = 'W';
     }
     return (
-      `${latHemisphere} ${latDegFloor} ${roundAndTruncate(latMinFloor, 2, 2)} ${roundAndTruncate(latSec, 2, 2)} ` +
-      `${lngHemisphere} ${lngDegFloor} ${roundAndTruncate(lngMinFloor, 2, 2)} ${roundAndTruncate(lngSec, 2, 2)}`
+      `${latHemisphere} ${Math.abs(this.latitudeWhole)} ${roundAndTruncate(latMinFloor, 2, 2)} ${roundAndTruncate(
+        latSec,
+        2,
+        2
+      )} ` +
+      `${lngHemisphere} ${Math.abs(this.longitudeWhole)} ${roundAndTruncate(lngMinFloor, 2, 2)} ${roundAndTruncate(
+        lngSec,
+        2,
+        2
+      )}`
     );
   }
 }
